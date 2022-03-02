836314a871cd7c89089a06485b9c26161a7d7fa3
fiqri aditya <72197739+fiqri-adt@users.noreply.github.com>
2022-03-02 19:10:52 +0700

    Responsif JS
    Responsif JS 3/2/22

---
+++ src/components/Footer/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  WebsiteRights,
+} from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us </FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> Contact Us </FooterLinkTitle>
+              <FooterLink to="/">Contact</FooterLink>
+              <FooterLink to="/">Support</FooterLink>
+              <FooterLink to="/">Destinations</FooterLink>
+              <FooterLink to="/">Sponsorships</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> Videos </FooterLinkTitle>
+              <FooterLink to="/">Submit Videos</FooterLink>
+              <FooterLink to="/">Ambassador</FooterLink>
+              <FooterLink to="/">Agency</FooterLink>
+              <FooterLink to="/">Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> Sosial Media </FooterLinkTitle>
+              <FooterLink to="/">Instagram</FooterLink>
+              <FooterLink to="/">Facebook</FooterLink>
+              <FooterLink to="/">Youtube</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <WebsiteRights>
+          Jago @ {new Date().getFullYear()} All rights reserved.
+        </WebsiteRights>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
